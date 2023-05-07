76c1a6f17b2e7d3a76002396e7fc5fbdd0f316b7
Nirajan Kunwor <nirajan.kunwor@beyondid.com>
2023-05-07 18:41:15 +0545

    day 10

---
+++ day10/scripts/script.js
@@ -0,0 +1,66 @@
+console.log('Hello world');
+// document object model
+document.write("hello world");
+
+// Javascript 8 data types
+
+// string
+
+// 'this is vrit class mern batch& * '
+// "true"
+// "1"
+
+//Number 1 2 3 4 no double/single quotes
+
+// BigInt large numbers
+
+//Boolean true/false
+
+//undefined
+
+// null
+
+// symbol
+
+// object [array, object ]
+//Key: value
+// {
+//    firstName: "samrat",
+//       lastName: "test",
+//          age: 23,
+//             isActive: true
+// }
+
+// 3 ways we can declare a variable
+// ES2015+ ES6 specification
+// var, let, const
+
+//camel case
+
+var a = "10";
+var b;
+
+//Pascal case
+
+let firstName = "Samrat";
+
+firstName = 10;
+
+firstName = true;
+
+//large data
+//json array of objects
+//object, array
+// Key: value
+firstName = {
+   batch: 1,
+   isActive: true
+}
+
+const array = [true, 12, "laxman", firstName];
+
+console.log(array)
+
+
+console.log(typeof firstName)
+console.log(typeof array)
